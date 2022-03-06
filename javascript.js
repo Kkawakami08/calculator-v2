87665163f592be2a0764b796b04394427279ff26
kkawakami08 <k.kawakami08@gmail.com>
2022-03-06 14:27:04 -0800

    eventlistener check

--- javascript.js
+++ javascript.js
@@ -1,14 +1,13 @@
 let a = 0;
 let b = 0;
+let result = 0;
 
 const add = function (a, b) {
-  let result;
   result = a + b;
   return result;
 };
 
 const subtract = function (a, b) {
-  let result;
   result = a - b;
   return result;
 };
@@ -20,24 +19,22 @@ const subtract = function (a, b) {
 // };
 
 const multiply = function (a, b) {
-  let result;
   result = a * b;
   return result;
 };
 
 const divide = function (a, b) {
-  let result;
   result = a / b;
   return result;
 };
 
 const power = function (a, b) {
-  let result = a ** b;
+  result = a ** b;
   return result;
 };
 
 const factorial = function (a) {
-  let result = 1;
+  result = 1;
   for (let i = 1; i <= a; i++) {
     result *= i;
   }
@@ -45,7 +42,6 @@ const factorial = function (a) {
 };
 
 let operator = "";
-let result = 0;
 
 const operate = function (operator, a, b) {
   if (operator === "add") {
@@ -59,4 +55,29 @@ const operate = function (operator, a, b) {
   }
 };
 
-console.log(operate("add", 5, 7));
+const sevenBtn = document.getElementById("seven");
+sevenBtn.value = 7;
+const eightBtn = document.getElementById("eight");
+const nineBtn = document.getElementById("nine");
+const fourBtn = document.getElementById("four");
+const fiveBtn = document.getElementById("five");
+const sixBtn = document.getElementById("six");
+const oneBtn = document.getElementById("one");
+const twoBtn = document.getElementById("two");
+const threeBtn = document.getElementById("three");
+const zeroBtn = document.getElementById("zero");
+
+const addBtn = document.getElementById("add");
+const subBtn = document.getElementById("sub");
+const multBtn = document.getElementById("mult");
+const divideBtn = document.getElementById("divide");
+const clearBtn = document.getElementById("clear");
+const equalBtn = document.getElementById("equal");
+
+const screenText = document.getElementById("screenDisplay");
+
+sevenBtn.addEventListener("click", display);
+
+function display(e) {
+  screenText.textContent = e.target.value;
+}
