c8d8b113e7d4aef491f1413c8461530d03854f10
kkawakami08 <k.kawakami08@gmail.com>
2022-03-07 17:09:00 -0800

    store operator and second value

--- javascript.js
+++ javascript.js
@@ -1,6 +1,6 @@
 let a = "";
 
-let b;
+let b = "";
 let result = 0;
 
 const add = function (a, b) {
@@ -90,16 +90,29 @@ threeBtn.addEventListener("click", store);
 zeroBtn.addEventListener("click", store);
 
 function store(e) {
-  let numberString = e.target.value.toString();
-  a += numberString;
-  console.log(a);
-  // a += e.target.value;
-  // console.log(a);
-  display(e);
+  if (operatorSign === "") {
+    let numberString = e.target.value.toString();
+    a += numberString;
+    console.log(a);
+    // a += e.target.value;
+    // console.log(a);
+    displayA(e);
+  } else {
+    let numberString = e.target.value.toString();
+    b += numberString;
+    console.log(b);
+    // a += e.target.value;
+    // console.log(a);
+    displayB(e);
+  }
 }
 
-function display(e) {
+function displayA(e) {
   screenText.textContent = a;
+}
+
+function displayB(e) {
+  screenText.textContent = b;
 }
 
 const addBtn = document.getElementById("add");
@@ -108,5 +121,27 @@ const multBtn = document.getElementById("mult");
 const divideBtn = document.getElementById("divide");
 const clearBtn = document.getElementById("clear");
 const equalBtn = document.getElementById("equal");
+
+addBtn.value = "+";
+subBtn.value = "-";
+multBtn.value = "*";
+divideBtn.value = "/";
+clearBtn.value = "clear";
+equalBtn.value = "equal";
+
+addBtn.addEventListener("click", storeOperator);
+subBtn.addEventListener("click", storeOperator);
+multBtn.addEventListener("click", storeOperator);
+divideBtn.addEventListener("click", storeOperator);
+clearBtn.addEventListener("click", storeOperator);
+equalBtn.addEventListener("click", storeOperator);
+
+let operatorSign = "";
+
+function storeOperator(e) {
+  operatorSign = e.target.value;
+  a = Number(a);
+  screenText.textContent = operatorSign;
+}
 
 const screenText = document.getElementById("screenDisplay");
